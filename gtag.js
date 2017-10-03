041657b6d56c884d29af555ff789cfca2961930d
survol <survol@users.noreply.github.com>
2017-10-02 18:33:15 -0700

    gtag remove second domain

--- gtag.js
+++ gtag.js
@@ -1,9 +1,13 @@
 <!-- Global Site Tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-101496449-2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-101496449-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments)};
   gtag('js', new Date());
 
-  gtag('config', 'UA-101496449-2');
+  gtag('config', 'UA-101496449-1' {
+    'linker': {
+      'domains': ['dreadzone.bitbucket.io', 'dreadzone.zone']
+    }
+  });
 </script>
